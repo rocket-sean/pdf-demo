c0b37a1d89c2c25edf8817af296fc0992cd1e5c6
Sean Buttinger <buttinger@rocket-apes.com>
2024-10-17 15:37:55 +0200

    PDF view experiments.

--- PDF Demo/PDFReaderViewController.swift
+++ PDF Demo/PDFReaderViewController.swift
@@ -10,19 +10,77 @@ import PDFKit
 
 
 class PDFReaderViewController: UIViewController {
+    
+    var pdfView: PDFView!
+    
+    
+    override var prefersStatusBarHidden: Bool {
+        true
+    }
+    
+    
+    override func loadView() {
+        
+        view = UIView()
+        
+        pdfView = PDFView()
+        pdfView.backgroundColor = .white
+        pdfView.autoScales = true
+        pdfView.translatesAutoresizingMaskIntoConstraints = false
 
+        view.addSubview(pdfView)
+        
+        NSLayoutConstraint.activate([
+            pdfView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            pdfView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            pdfView.topAnchor.constraint(equalTo: view.topAnchor),
+            pdfView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let pdfView = PDFView()
-        pdfView.backgroundColor = .white
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(handlePDFViewPageChangedNotification(notification:)),
+            name: Notification.Name.PDFViewPageChanged,
+            object: pdfView)
         
-        pdfView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(pdfView)
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(handlePDFViewSelectionChangedNotification(notification:)),
+            name: Notification.Name.PDFViewSelectionChanged,
+            object: pdfView)
         
-        pdfView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        pdfView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        pdfView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        pdfView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        pdfView.document = loadDocument(named: "thinkos")
+    }
+    
+    
+    private func loadDocument(named name: String) -> PDFDocument? {
+        
+        guard let path = Bundle.main.url(forResource: name, withExtension: "pdf"),
+              let document = PDFDocument(url: path) else {
+            return nil
+        }
+        
+        return document
+    }
+    
+    
+    @objc private func handlePDFViewPageChangedNotification(notification: Notification) {
+        
+        if let pageNumber = pdfView.document?.index(for: pdfView.currentPage!) {
+            print("PAGE: \(pageNumber)")
+        }
+    }
+        
+    
+    @objc private func handlePDFViewSelectionChangedNotification(notification: Notification) {
+
+        if let selectedText = pdfView.currentSelection?.string {
+            print("SELECTION: \(selectedText)")
+        }
     }
 }
